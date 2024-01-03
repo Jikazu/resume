0b22f706233bb028b3422665ccab24078f978b22
Gabriella Köhler <gabriella.kohler91@gmail.com>
2024-01-03 12:27:09 +0000

    added so that the form is connected to the function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+function sendMail(contactForm) {
+    emailjs.send("service_r2zdchp", "template_ohjy575", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        }, 
+        function(error) {
+            console.log("FAILED", error);
+        });
+}
